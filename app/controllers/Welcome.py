b7fcb6d700014ed8bb43d40391db53e4d5bff1a7
Dustin Brown <Dustin Brown>
2016-06-10 16:00:20 -0400

    add gold functionality and routes

--- app/controllers/Welcome.py
+++ app/controllers/Welcome.py
@@ -7,34 +7,45 @@
     Create a controller using this template
 """
 from system.core.controller import *
+from random import randrange
+from time import strftime
+from datetime import datetime
+
+search_settings = {
+    'farm': {'min': 10 , 'max': 20},
+    'cave': {'min': 5, 'max': 10},
+    'house': {'min': 2, 'max': 5},
+    'casino': {'min': -50, 'max': 50},
+    }
 
 class Welcome(Controller):
     def __init__(self, action):
         super(Welcome, self).__init__(action)
-        """
-            This is an example of loading a model.
-            Every controller has access to the load_model method.
-        """
-        self.load_model('WelcomeModel')
-        self.db = self._app.db
-
-        """
         
-        This is an example of a controller method that will load a view for the client 
-
-        """
    
     def index(self):
-        """
-        A loaded model is accessible through the models attribute 
-        self.models['WelcomeModel'].get_users()
+        if not 'total_gold' in session:
+            session['total_gold'] = 0
+            session['activity-feed'] =[]
         
-        self.models['WelcomeModel'].add_message()
-        # messages = self.models['WelcomeModel'].grab_messages()
-        # user = self.models['WelcomeModel'].get_user()
-        # to pass information on to a view it's the same as it was with Flask
-        
-        # return self.load_view('index.html', messages=messages, user=user)
-        """
         return self.load_view('index.html')
-
+    def process_money(self):
+        location = request.form['location']
+        range_max = search_settings[location]['max']
+        range_min = search_settings[location]['min']
+        rand_gold = randrange(range_min, range_max + 1)
+        session['total_gold'] += rand_gold
+        current_time =datetime.now().strftime("%Y-%m-%d %I:%M%p")
+        if rand_gold < 0:
+            text_color = 'red'
+            sentence = "Entered a {} and lost {} golds... Ouch... ({})".format(location,rand_gold * -1, current_time)
+        else:
+            text_color = 'green'
+            sentence = "Earned {} golds from the {}! ({})".format(rand_gold, location, current_time)
+            outcome = {
+            'description': sentence,
+            'text_color': text_color
+            }
+            session['activity-feed'].insert(0, outcome)
+            return redirect('/')
+        return redirect('/')
